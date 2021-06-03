deb20d49548c9fd03ca69a55f69f3ea6d04522f0
Huska, Matthew <HuskaM@rki.de>
2021-06-03 19:17:38 +0200

    Updating summarize.R script. Not tested yet

--- src/summarize.R
+++ src/summarize.R
@@ -4,8 +4,10 @@ library(optparse)
 library(digest)
 
 option_list <- list(
-    make_option(c("-i", "--input"), default="",
-                help="File with clustering and PLZ, SMAPLING_DATE, etc [default %default]"),
+    make_option(c("-i", "--input"), default="clusters.tsv",
+                help="File with clustered sequences. Contains sequence name and cluster_id."),
+    make_option(c("-m", "--metadata"), default="desh.tsv",
+                help="Metadata for clustered sequences"),
     make_option(c("-o", "--outdir"), default="output/",
                 help="Output directory [default %default]")
     )
@@ -18,19 +20,23 @@ library(readr, warn.conflicts = FALSE)
 if (!dir.exists(args$outdir))
   dir.create(args$outdir, recursive = TRUE, showWarnings = FALSE)
 
-x <- read.delim(args$input)
-#x %>% filter(!is.na(cluster_id)) %>% arrange(cluster_id, desc(SAMPLING_DATE)) %>% head()
-cluster_dates <- x %>%
+clusters <- read.delim(args$input)
+meta <- read.delim(args$metadata)
+
+cluster_summary <- clusters %>%
+  filter(nchar(cluster_id) > 0) %>%
+  left_join(meta, by = c("accession" = "ID")) %>%
   mutate(SAMPLING_DATE2 = ymd(SAMPLING_DATE)) %>%
   group_by(cluster_id) %>%
   summarize(sampling_date_min = min(SAMPLING_DATE2), sampling_date_max = max(SAMPLING_DATE2), sampling_date_range_days = time_length(interval(min(SAMPLING_DATE2), max(SAMPLING_DATE2)), "day"), cluster_size = n(), frac_top_plz = max(table(PRIMARY_DIAGNOSTIC_LAB_PLZ), na.rm = TRUE) / n()) %>%
   mutate(cluster_score = (cluster_size + 1)/ (sampling_date_range_days + 1)) %>%
   arrange(desc(cluster_score))
 
-print(cluster_dates)
+write_tsv(cluster_summary, file.path(args$outdir, "cluster-summary-all.tsv"))
 
 # Recent only
-recent_cluster_dates <- cluster_dates %>% filter(sampling_date_max >= today() - weeks(2))
+recent_cluster_dates <- cluster_summary %>% filter(sampling_date_max >= today() - days(14))
+write_tsv(recent_cluster_dates, file.path(args$outdir, "cluster-summary-last-14-days.tsv"))
 
-write_tsv(cluster_dates, file.path(args$outdir, "cluster-summary.tsv"))
-write_tsv(recent_cluster_dates, file.path(args$outdir, "cluster-summary-last-two-weeks.tsv"))
+recent_cluster_dates <- cluster_summary %>% filter(sampling_date_max >= today() - days(7))
+write_tsv(recent_cluster_dates, file.path(args$outdir, "cluster-summary-last-7-days.tsv"))
