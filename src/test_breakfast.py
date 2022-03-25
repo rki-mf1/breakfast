2ef2b8d234f7ea57043e3bd51a4a8e72438a9d60
denisbeslic <denis.beslic@outlook.com>
2022-03-25 16:10:27 +0100

    Add test function and files to test caching functionality

--- src/test_breakfast.py
+++ src/test_breakfast.py
@@ -35,4 +35,80 @@ def test_clustering_dist1_noskipdel():
     output_file = 'test/clusters.tsv'
     e = open(expected_file, "r")
     o = open(output_file, "r")
-    assert e.read() == o.read()    
+    assert e.read() == o.read()
+
+# caching tests
+
+def test_caching_export():
+    os.system('python src/breakfast.py --input-file test/testfile.tsv --outdir test/ --max-dist 1 --output-cache test/testfile.json')
+    assert exit_status == 0
+    assert os.path.exists('test/testfile.json')
+
+def test_caching_import():
+    os.system('python src/breakfast.py --input-file test/testfile_caching01_AddedSeqs.tsv --outdir test/ --max-dist 1 --input-cache test/testfile.json')
+    assert exit_status == 0
+
+def test_caching_01_AddingSequences():
+    os.system('python src/breakfast.py --input-file test/testfile_caching01_AddedSeqs.tsv --outdir test/ --max-dist 1 --input-cache test/testfile.json')
+    expected_file = 'test/expected_clusters_caching01_dist1.tsv'
+    output_file = 'test/clusters.tsv'
+    e = open(expected_file, "r")
+    o = open(output_file, "r")
+    assert e.read() == o.read()
+
+def test_caching_02_DisorderedSequences():
+    os.system('python src/breakfast.py --input-file test/testfile_caching02_DisorderedOnly.tsv --outdir test/ --max-dist 1 --input-cache test/testfile.json')
+    expected_file = 'test/expected_clusters_caching02_dist1.tsv'
+    output_file = 'test/clusters.tsv'
+    e = open(expected_file, "r")
+    o = open(output_file, "r")
+    assert e.read() == o.read()
+
+def test_caching_03_AddedAndDisorderedSequences():
+    os.system('python src/breakfast.py --input-file test/testfile_caching03_AddedAndDisorderedSeqs.tsv --outdir test/ --max-dist 1 --input-cache test/testfile.json')
+    expected_file = 'test/expected_clusters_caching03_dist1.tsv'
+    output_file = 'test/clusters.tsv'
+    e = open(expected_file, "r")
+    o = open(output_file, "r")
+    assert e.read() == o.read()
+
+def test_caching_04_DeletingSingleSequences():
+    os.system('python src/breakfast.py --input-file test/testfile_caching04_DeletedSingleSeq.tsv --outdir test/ --max-dist 1 --input-cache test/testfile.json')
+    expected_file = 'test/expected_clusters_caching04_dist1.tsv'
+    output_file = 'test/clusters.tsv'
+    e = open(expected_file, "r")
+    o = open(output_file, "r")
+    assert e.read() == o.read()
+
+# Complete Profile gets deleted
+def test_caching_05_DeletedMultipleSequences():
+    os.system('python src/breakfast.py --input-file test/testfile_caching05_DeletedProfile.tsv --outdir test/ --max-dist 1 --input-cache test/testfile.json')
+    expected_file = 'test/expected_clusters_caching05_dist1.tsv'
+    output_file = 'test/clusters.tsv'
+    e = open(expected_file, "r")
+    o = open(output_file, "r")
+    assert e.read() == o.read()
+
+def test_caching_06_ModifiedSequences():
+    os.system('python src/breakfast.py --input-file test/testfile_caching06_ModifiedSeqs.tsv --outdir test/ --max-dist 1 --input-cache test/testfile.json')
+    expected_file = 'test/expected_clusters_caching06_dist1.tsv'
+    output_file = 'test/clusters.tsv'
+    e = open(expected_file, "r")
+    o = open(output_file, "r")
+    assert e.read() == o.read()
+
+def test_caching_07_MultipleTests():
+    os.system('python src/breakfast.py --input-file test/testfile_caching07_MultipleTests.tsv --outdir test/ --max-dist 1 --input-cache test/testfile.json')
+    expected_file = 'test/expected_clusters_caching07_dist1.tsv'
+    output_file = 'test/clusters.tsv'
+    e = open(expected_file, "r")
+    o = open(output_file, "r")
+    assert e.read() == o.read()
+
+def test_caching_08_MultipleTests():
+    os.system('python src/breakfast.py --input-file test/testfile_caching08_NoChanges.tsv --outdir test/ --max-dist 1 --input-cache test/testfile.json')
+    expected_file = 'test/expected_clusters_caching08_dist1.tsv'
+    output_file = 'test/clusters.tsv'
+    e = open(expected_file, "r")
+    o = open(output_file, "r")
+    assert e.read() == o.read()  
