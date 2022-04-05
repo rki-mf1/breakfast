eaf58c85704ce1f80dad441057062dc239efa336
denisbeslic <denis.beslic@outlook.com>
2022-04-05 17:52:07 +0200

    Add pairwise fast cython module. Currently slower than imported sklearn module

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from Cython.Build import cythonize
+import numpy
+
+setup(
+    name='_pairwise_fast',
+    ext_modules=cythonize("_pairwise_fast.pyx"),
+    zip_safe=False,
+    include_dirs=[numpy.get_include()],
+    extra_compile_args=['-fopenmp'],
+    extra_link_args=['-fopenmp'],
+)
+
