0c243f6ece54c1f4e0939d0b1be80cbd690ee3c6
Huska, Matthew <HuskaM@rki.de>
2022-05-09 15:06:44 +0200

    Add black and flake8 as development deps, add pre-commit hooks

--- noxfile.py
+++ noxfile.py
@@ -67,7 +67,5 @@ def lint(session: Session) -> None:
 def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", "--no-dev", external=True)
-    install_with_constraints(
-        session, "coverage[toml]", "pytest", "pytest-cov"
-    )
+    install_with_constraints(session, "coverage[toml]", "pytest", "pytest-cov")
     session.run("pytest", *args)
