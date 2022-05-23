9c5225f760cc7d1813951cfea3400cf436d236ff
Huska, Matthew <HuskaM@rki.de>
2022-05-23 17:31:15 +0200

    Use safety to auto check for insecure deps

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,7 @@ from nox.sessions import Session
 
 
 package = "breakfast"
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "lint", "safety", "tests"
 locations = "src", "tests", "noxfile.py"
 
 
@@ -61,6 +61,23 @@ def lint(session: Session) -> None:
         "darglint",
     )
     session.run("flake8", *args)
+
+
+@nox.session(python="3.10")
+def safety(session: Session) -> None:
+    """Scan dependencies for insecure packages."""
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        install_with_constraints(session, "safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
 
 
 @nox.session(python=["3.10"])
